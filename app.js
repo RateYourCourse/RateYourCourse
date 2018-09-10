e456599672015233225f9fa9bb792dee5fe98b3c
Evelyne Erni <evi.erni@hotmail.com>
2018-09-11 00:15:20 +0200

    Update crawler and app

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //variables
 const MongoClient = require("mongodb").MongoClient;
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const url = "mongodb://localhost:27017";
 
@@ -13,10 +16,6 @@ app.use(express.static("static"));
 
 //Implementation of a search function
 app.get("/courses", function(req, res) {
-  //var courseId = req.query.id ? req.query.id : {};
-  //var courseName = req.query.name ? req.query.name : {};
-  //var courseProf = req.query.prof ? req.query.prof : {};
-  // const name = req.query.name || req.query.prof || req.query.id || "";
   const name = req.query.q || "";
 
   db.collection("Courses")
@@ -28,12 +27,15 @@ app.get("/courses", function(req, res) {
       res.send(JSON.stringify(result));
     });
 });
-
+app.post("/reviews", function(req, res) {
+  var data = req.body.data;
+  console.log(data);
+});
 MongoClient.connect(
   url,
   function(err, client) {
     db = client.db(dbName);
-    db.collection("Courses").createIndex({ "$*": "text" });
+    db.collection("Courses").createIndex({ "$**": "text" });
     app.listen(3000, function() {
       console.log("Example app listening on port 3000!");
     });
